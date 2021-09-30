c63d1935c4c5f9e781a3d1f2b13cf64f3a429865
BryantCR <bryancvargas20@gmail.com>
2021-09-29 22:34:43 -0600

    wuuu

--- users_app/models/User.py
+++ users_app/models/User.py
@@ -22,3 +22,41 @@ class User:
         query = "INSERT INTO users (first_name , last_name , email, created_at, updated_at) VALUES ( %(first_name)s , %(last_name)s , %(email)s, SYSDATE(), SYSDATE());"
         result = connectToMySQL('users_shema').query_db(query,data)
         return result
+
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+
+
+    # @classmethod
+    # def get_all(cls):
+    #     query = "SELECT * FROM users;"
+    #     results = connectToMySQL('users_schema').query_db(query)
+    #     users = []
+    #     for u in results:
+    #         users.append( cls(u) )
+    #     return users
+
+    # @classmethod
+    # def save(cls, data):
+    #     query = "INSERT INTO users (first_name,last_name,email) VALUES (%(first_name)s,%(last_name)s,%(email)s);"
+
+    #     # comes back as the new row id
+    #     result = connectToMySQL('users_schema').query_db(query,data)
+    #     return result
+
+    # @classmethod
+    # def get_one(cls,data):
+    #     query  = "SELECT * FROM users WHERE id = %(id)s";
+    #     result = connectToMySQL('users_schema').query_db(query,data)
+    #     return cls(result[0])
+
+    # @classmethod
+    # def update(cls,data):
+    #     query = "UPDATE users SET first_name=%(first_name)s,last_name=%(last_name)s,email=%(email)s,updated_at=NOW() WHERE id = %(id)s;"
+    #     return connectToMySQL('users_schema').query_db(query,data)
+
+    # @classmethod
+    # def destroy(cls,data):
+    #     query  = "DELETE FROM users WHERE id = %(id)s;"
+    #     return connectToMySQL('users_schema').query_db(query,data)
